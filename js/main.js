234163a119fe976be956450f80bed906e9ddca2e
Saul Suaste <sumsaul94@hotmail.com>
2021-08-17 16:25:18 -0500

    Seccion home terminada, css y js. js para nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-// alert("hola");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        }else{
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    // Palanca (toggle) para el boton menu
+    $(".menu-btn").click(function(){
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");//poner el X
+    });
+
+});
