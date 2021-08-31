3c6dd0b3675ff100862ce7fa322d0b446335dd98
Saul Suaste <sumsaul94@hotmail.com>
2021-08-31 17:14:38 -0500

    seccion Equipo, html, css y js

--- js/main.js
+++ js/main.js
@@ -13,4 +13,26 @@ $(document).ready(function(){
         $(".menu-btn i").toggleClass("active");//poner el X
     });
 
+    // Owl Carousel
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
