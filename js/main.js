8da4458c427e491e664b86732e2fd2e5d8ef80b8
Saul Suaste <sumsaul94@hotmail.com>
2021-08-31 19:41:19 -0500

    potafolioPro terminado

--- js/main.js
+++ js/main.js
@@ -5,12 +5,40 @@ $(document).ready(function(){
         }else{
             $(".navbar").removeClass("sticky");
         }
+
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        }else{
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
+
+    // animacion de boton scroll up
+    $(".scroll-up-btn").click(function(){
+        $("html").animate({scrollTop: 0})
     });
 
     // Palanca (toggle) para el boton menu
     $(".menu-btn").click(function(){
         $(".navbar .menu").toggleClass("active");
         $(".menu-btn i").toggleClass("active");//poner el X
+    });
+
+
+    // Cambio de palabrasen inicio
+    var typed = new Typed(".typing", {
+        strings: ["Front Developer","Desarrollador", "Diseñador WEB", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // Cambio de palabras en acerca de
+    var typed = new Typed(".typing2", {
+        strings: ["Front Developer","Desarrollador", "Diseñador WEB", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // Owl Carousel
